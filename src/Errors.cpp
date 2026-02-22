c900f4398ec18e5e515932a56842f4949dcc13ab
doktorov-av <doktorov.alexander.01@mail.ru>
2026-02-22 16:29:22 +0300

    ref: removed unused defines

--- src/Errors.cpp
+++ src/Errors.cpp
@@ -3,15 +3,7 @@
 //
 
 #include "Errors.hpp"
-
 #include <system_error>
-
-#ifdef WIN32
-#define strerror_length(error) 100
-#else
-#define strerror_length(error) strerrorlen_s(errno) + 1
-#endif
-
 
 std::string plifaces::SystemErrorMessage() {
     const auto errorCode = std::make_error_code(static_cast<std::errc>(errno));
